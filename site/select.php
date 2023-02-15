aa1f4a3a30d07fcc1c818957a7178838a4d1fb37
Mohamad Mohamad <mmohamad115@student.novacollege.nl>
2023-02-15 09:13:40 +0100

    opdracht 21

---
+++ site/select.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="show.php">Show zonder waarde</a>
+            </li>
+            <li>
+                <a href="show.php?waarde1=Honderd">Show met 1 waarde</a>
+            </li>
+            <li>
+                <a href="show.php?waarde1=TweeHonderd&waarde2=Drie">Show met 2 waardes</a>
+            </li>
+            <li>
+                <a href="show.php?waarde1=Snoop%20Dog&waarde2=Dr%20Dre">Snoop dog en Dr Dre</a>
+            </li>
+        </ul>
+    </nav>
+</body>
+</html>
