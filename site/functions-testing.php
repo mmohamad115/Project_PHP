4c5cd6138d8013ebb45b8c7b05c7b9f38a1f83d5
Mohamad Mohamad <mmohamad115@student.novacollege.nl>
2023-02-15 15:32:13 +0100

    week_3_opdr_1

---
+++ site/functions-testing.php
@@ -0,0 +1,22 @@
+<?php
+
+$naam = "";
+$leeftijd = 0;
+$isGetrouwd = false;
+$kinderen = array();
+if(empty($naam)){
+    echo "<br>";
+    var_dump (empty($naam));
+}
+if(empty($leeftijd)){
+    echo "<br>";
+    var_dump (empty($leeftijd));
+}
+if(empty($isGetrouwd)){
+    echo "<br>";
+    var_dump (empty($isGetrouwd));
+}
+if(empty($isGetrouwd)){
+    echo "<br>";
+    var_dump (empty($kinderen));
+}
--- site/show.php
+++ site/show.php
@@ -1,6 +1,5 @@
 <?php
-echo"<pre>";
+
 echo $_GET["Naam"];
 echo $_GET["Achternaam"];
 echo $_GET["Leeftijd"];
-echo "</pre>";
